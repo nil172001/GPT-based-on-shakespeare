9e6b4ca3e154886588259c59075eb22493b0a993
nil172001 <matarollobetn@gmail.com>
2025-06-18 15:15:19 +0200

    next step

--- training.py
+++ training.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+#hyperparameters
+batch_size=32
+block_size=8
+max_iters=3000
+eval_interval=300
+learning_rate=1e-2
+device="cuda" if torch.cuda.is_available() else "cpu"
+eval_iters=200
+#-------------------
+torch.manual_seed(1337)
 
 # I downloaded input.txt from this guys github karpathy
 with open("input.txt","r", encoding="utf-8") as f:      #encoding='utf-8'since python stores text as bytes, it needs to be able to interpret them and not make mistakes
@@ -57,9 +67,24 @@ def get_batch(split):
     ix = torch.randint(len(data)-block_size,(batch_size,))
     x=torch.stack([data[i:i+block_size] for i in ix])
     y=torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x,y=x.to(device), y.to(device)
     return x,y
 
-xb,yb=get_batch("train")
+@torch.no_grad()
+def estimate_loss():
+    out={}
+    model.eval() #evaluation phase (at the moment doesnt do anything)
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X,Y=get_batch(split)
+            logits,loss=model(X,Y)
+            losses[k]=loss.item()
+        out[split]=losses.mean()
+    model.train() #training phase (but its good practice, since advanced NN will have diferent phases)
+    return out
+
+#xb,yb=get_batch("train")
 '''
 print("inputs:")
 print(xb.shape)
@@ -76,17 +101,57 @@ class BigramLanguageModel(nn.Module):
         #each token reads from logits for the next token from a lookup table
         self.token_embedding_table=nn.Embedding(vocab_size,vocab_size)
 
-    def forward(self,idx,targets):
+    def forward(self,idx,targets=None):
         #idx and targets are both (B,T) tensor of integers
         logits=self.token_embedding_table(idx) #(B,T,C) batch, time,channel
-        B,T,C=logits.shape
-        logits=logits.view(B*T,C) #this becomes 2 dimension so it can work on the cross_entropy
-        targets=targets.view(B*T) #1D , if u put -1 it will do the same
-        loss=F.cross_entropy(logits,targets) #torch.nn.f library (wants the second input to be the channel)
+
+        if targets is None:
+            loss=None
+        else:
+            B,T,C=logits.shape
+            logits=logits.view(B*T,C) #this becomes 2 dimension so it can work on the cross_entropy
+            targets=targets.view(B*T) #1D , if u put -1 it will do the same
+            loss=F.cross_entropy(logits,targets) #torch.nn.f library (wants the second input to be the channel)
 
         return logits, loss
-m=BigramLanguageModel(vocab_size)
-logits,loss=m(xb,yb)
-print(logits.shape)
-print(loss)
+    def generate(self,idx,max_new_tokens):
+        #idx is (B,T) array of inidices in the current context
+        for _ in range(max_new_tokens):
+            #get the predictions
+            logits, loss =self(idx)
+            #focus only on the last time step
+            logits = logits[:,-1,:] #beconmes (B,C)
+            #apply softmax to get probabilities
+            probs =F.softmax(logits, dim=-1)#(B,C)
+            #sample from the distribution
+            idx_next =torch.multinomial(probs,num_samples=1) #(B,1)
+            #append sampled index to the running sequence
+            idx =torch.cat((idx,idx_next),dim=1) #(B,T+1)
+        return idx
+model=BigramLanguageModel(vocab_size)
+m=model.to(device)
+#logits,loss=m(xb,yb)
+#print(logits.shape)
+#print(loss) #we expect a loss of -ln(1/65) =4.17 so we have a bit of loss
 
+#print(decode(m.generate(idx=torch.zeros((1,1),dtype=torch.long),max_new_tokens=100)[0].tolist()))
+
+#create pytorch optimizer ADAM
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+for iter in range(max_iters):
+    #every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    #sample a batch of data
+    xb,yb=get_batch("train")
+    #evaluate the loss
+    logits,loss=m(xb,yb)
+    optimizer.zero_grad(set_to_none=True) #zeroing all the gradients
+    loss.backward() #getting the gradients from all the parameters
+    optimizer.step() #optimizing the parameters
+    #print(loss.item())
+context=torch.zeros((1,1),dtype=torch.long, device=device)
+print(decode(m.generate(context,max_new_tokens=100)[0].tolist()))
